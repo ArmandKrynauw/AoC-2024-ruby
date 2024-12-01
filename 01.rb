2f386a303f8dc387a3a8c40e04afed3cc1803790
Armand Krynauw <armand.krynauw@gmail.com>
2024-12-01 12:36:16 +0200

    Add Day 01, Part 2

--- 01.rb
+++ 01.rb
@@ -6,3 +6,11 @@ def part1(input)
   list2 = pairs.map(&:second).sort
   list1.zip(list2).map { |p| (p.first - p.second).abs }.sum
 end
+
+def part2(input)
+  pairs = input.split("\n").map { |l| l.split.map(&:to_i) }
+  list1 = pairs.map(&:first)
+  list2 = pairs.map(&:second)
+  tally = list2.tally
+  list1.map { |v| v * (tally[v] || 0) }.sum
+end
